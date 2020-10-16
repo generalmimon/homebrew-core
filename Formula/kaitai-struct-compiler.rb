9f8ecdfeec858d2eab775003a8c85c1976ca53bf
Petr Pučil <petr.pucil@seznam.cz>
2020-10-16 16:44:05 +0200

    kaitai-struct-compiler 0.9
    See https://github.com/Homebrew/homebrew-core/pull/23712, which updated kaitai-struct-compiler to 0.8

--- Formula/kaitai-struct-compiler.rb
+++ Formula/kaitai-struct-compiler.rb
@@ -1,8 +1,8 @@
 class KaitaiStructCompiler < Formula
   desc "Compiler for generating binary data parsers"
   homepage "https://kaitai.io/"
-  url "https://bintray.com/artifact/download/kaitai-io/universal/0.8/kaitai-struct-compiler-0.8.zip"
-  sha256 "545fc10e134db2901cad8817be1b440fca6f2bad8b92b2948ebe0647f3ffa2c9"
+  url "https://bintray.com/artifact/download/kaitai-io/universal/0.9/kaitai-struct-compiler-0.9.zip"
+  sha256 "3038243334fb65bbb264f33b82986facfe1fbad2de1978766899855b40212215"
   revision 2
 
   bottle :unneeded
